91e49a3883e265399a0932af4653ed526ba76f13
Troogle <sqh1994@163.com>
2015-02-10 03:08:59 +0800

    final(real)

--- web/ctb/views.py
+++ web/ctb/views.py
@@ -1,4 +1,4 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 def home(request):
-    return render('index.html')
+    return render_to_response('index.html',{})
--- web/settings/__init__.py
+++ web/settings/__init__.py
@@ -68,7 +68,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(HERE, '../static'),
+    os.path.join(HERE, '../../static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'settings.urls'
 WSGI_APPLICATION = 'settings.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(HERE, '../templates'),
+    os.path.join(HERE, '../../templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
