2505c148a7cdd3ffc66ce2fbe88aae27a52fbc27
Troogle <sqh1994@163.com>
2015-02-10 03:03:33 +0800

    today final

--- web/settings/urls.py
+++ web/settings/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('ctb',
+url(r'^$', 'views.home'),
     # Examples:
     # url(r'^$', 'web.views.home', name='home'),
     # url(r'^web/', include('web.foo.urls')),
