5addbd54fbc6d964ed3be1566bc3ffd5e89be801
Troogle <sqh1994@163.com>
2015-02-10 13:28:38 +0800

    working and base

--- web/ctb/views.py
+++ web/ctb/views.py
@@ -2,3 +2,15 @@
 from django.shortcuts import render_to_response
 def home(request):
     return render_to_response('index.html',{})
+def rules(request):
+    return render_to_response('working.html',{})
+def pool(request):
+    return render_to_response('working.html',{})
+def statistics(request):
+    return render_to_response('working.html',{})
+def staffs(request):
+    return render_to_response('working.html',{})
+def support(request):
+    return render_to_response('working.html',{})
+def register(request):
+    return render_to_response('working.html',{})
--- web/settings/urls.py
+++ web/settings/urls.py
@@ -5,14 +5,14 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('ctb',
-url(r'^$', 'views.home'),
+url(r'^$', 'views.home',name='home'),
+url(r'/rules^$', 'views.rules',name='rules'),
+url(r'/pool^$', 'views.pool',name='pool'),
+url(r'/pool^$', 'views.statistics',name='statistics'),
+url(r'/pool^$', 'views.staffs',name='staffs'),
+url(r'/pool^$', 'views.support',name='support'),
+url(r'/pool^$', 'views.register',name='register'),
     # Examples:
     # url(r'^$', 'web.views.home', name='home'),
     # url(r'^web/', include('web.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
