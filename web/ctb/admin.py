9ccb271d8a599cdfe0351b329dcd1e464c5fa715
Troogle <sqh1994@163.com>
2015-02-12 14:08:35 +0800

    check user

--- web/ctb/admin.py
+++ web/ctb/admin.py
@@ -4,7 +4,16 @@ from .models import Match
 from .models import Beatmap
 
 # Register your models here.
+def checkuser(modeladmin, request, queryset):
+    queryset.update(checked=True)
+    checkuser.short_description = "Mark selected user checked"
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'checked','userType']
+    ordering = ['username']
+    actions = [checkuser]
 
 admin.site.register(Match)
-admin.site.register(MatchUser)
+admin.site.register(MatchUser,UserAdmin)
 admin.site.register(Beatmap)
+
